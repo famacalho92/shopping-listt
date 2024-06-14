c2c9c082526bd3a00e59be80cb642484019f897e
Ivan Castillo <famacalho@unicauca.edu.co>
2024-06-14 13:16:39 -0400

    adding registro

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component} from '@angular/core';
+import {
+  FormGroup, 
+  FormControl,
+  Validators,
+  FormBuilder
+} from '@angular/forms';
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.page.html',
   styleUrls: ['./registro.page.scss'],
 })
-export class RegistroPage implements OnInit {
+export class RegistroPage    {
+  
+  formularioRegistro: FormGroup;
 
-  constructor() { }
+  constructor(public fb: FormBuilder) {
+    this.formularioRegistro = this.fb.group({
+      'nombre': new FormControl ("", Validators.required),
+      'password': new FormControl("", Validators.required),
+      'confirmacionPassword': new FormControl("", Validators.required)
+    });
+   }
 
-  ngOnInit() {
-  }
 
 }
