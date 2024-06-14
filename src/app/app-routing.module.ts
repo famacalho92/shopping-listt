c13e8c7b848e143fb06986152e64c4641044c730
Ivan Castillo <famacalho@unicauca.edu.co>
2024-06-14 01:55:19 -0400

    Frontend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,23 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { 
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+
+  },
   { path: '', redirectTo: 'lista-compras', pathMatch: 'full' },
   { path: 'lista-compras', loadChildren: () => import('./pages/lista-compras/lista-compras.module').then(m => m.ListaComprasPageModule) },
-  { path: 'lista-detalle/:id', loadChildren: () => import('./pages/lista-detalle/lista-detalle.module').then(m => m.ListaDetallePageModule) }
+  { path: 'lista-detalle/:id', loadChildren: () => import('./pages/lista-detalle/lista-detalle.module').then(m => m.ListaDetallePageModule) },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'registro',
+    loadChildren: () => import('./registro/registro.module').then( m => m.RegistroPageModule)
+  }
 ];
 
 @NgModule({
